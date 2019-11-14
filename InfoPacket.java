97523adc762ae755db7dabd4ef5fd602bea09be4
rymndlcnn <35828910+rymndlcnn@users.noreply.github.com>
2019-11-14 17:12:06 +0800

    created variables and constructor in InfoPacket

--- InfoPacket.java
+++ InfoPacket.java
@@ -3,7 +3,18 @@ import java.net.*;
 import java.util.*;
 
 public class InfoPacket {
+	// variable declaration
+	private int player_id;
+    private String action;
+    private String card_rank;
+    private String card_suit;
 
-	private 
+	// Constructor
+    public InfoPacket(int player_id, String action, String card_rank, String card_suit){
+        this.player_id = player_id;
+        this.action = action;
+        this.card_rank = card_rank;
+        this.card_suit = card_suit;
+	}
 
 }
