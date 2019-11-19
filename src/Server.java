f796c34600e7f696df27bf90496fd7fb925bf952
cjldadios <cjldadios.uplb@gmail.com>
2019-11-19 16:22:24 +0800

    added read and send byte array

--- src/Server.java
+++ src/Server.java
@@ -1,21 +1,35 @@
+/**
+* Documentations
+*/
 
 import java.util.ArrayList;
-import jav.lang.Thread;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
 
-public class Server {
+public class Server extends Player implements Runnable {
+	private static final int SECONDS = 1000;
 
-	private ServerSocket server_socket;
-	private ArrayList<Socket> socket_connections = new ArrayList<Socket>();
-	private static int default_port = 8080;
-	private static int client_capacity = 2;
+	private static int defaultPort = 8080;
+	private static int maxPlayers = 13;
+	private static int timeout = 60 * SECONDS; 
 
-	/*Server has */
-	private ArrayList<Thread> player_thread_list = new ArrayList<Thread>();
+	private ServerSocket serverSocket;
+	
+	private ArrayList<Thread> playerThreadList = new ArrayList<Thread>(13);
+	private ArrayList<Socket> socketConnectionList = new ArrayList<Socket>(13);
 
-	public Server() {
+    public Server() throws IOException {
+        // binding a socket to a port
+        serverSocket = new ServerSocket(this.defaultPort);
+        serverSocket.setSoTimeout( this.timeout);
+
+
+    }
+
+    @Override
+	public void run() {
 		
 	}
-
-	
 
 }
